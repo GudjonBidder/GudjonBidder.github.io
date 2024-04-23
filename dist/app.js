ca0246595ef6004b7947b4ba4990998915c285ae
walidmahade <walidmahade@gmail.com>
2024-04-23 21:23:30 +0600

    Auto commit

--- dist/app.js
+++ dist/app.js
@@ -862,7 +862,7 @@ $(function() {
             // update price and links and rating
             $offer_card.find(".price_text-total").text(item.total + " nok");
             $offer_card.find(".continue_button").attr("href", item.link);
-            $offer_card.find(".average-price_text").text(Math.round(item.total) + " nok/mo. for 24 mo");
+            $offer_card.find(".average-price_text").text(Math.round(item.total) + " nok per m\xe5ned");
             // update rating number
             const rating = 5 - i < 2 ? 2 : 5 - i;
             $offer_card.find(".rating_text").text(rating + "/5");
@@ -874,7 +874,6 @@ $(function() {
         });
         // show hide services
         const $buttonServices = $(".button-services");
-        console.log("$buttonServices", $buttonServices);
         $buttonServices.on("click", function() {
             const $el = $(this);
             const $services = $el.closest(".offer_card").find(".operator-details").first();
